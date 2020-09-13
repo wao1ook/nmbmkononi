eef4516104585b8a86ad0afc24a1e9006648ec87
James Emanuel <jamesemanuel99@gmail.com>
2020-09-13 11:22:41 +0300

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:nmbmkononi/pages/home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: App(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Home(),
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 1,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            child: Column(
+              children: <Widget>[
+                Home(),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
