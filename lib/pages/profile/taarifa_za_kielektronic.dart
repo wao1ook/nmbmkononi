19387d6c9f9a4ae1c8078d238f9d1c518a81220e
James Emanuel <jamesemanuel99@gmail.com>
2020-09-16 19:28:40 +0300

    updates: TaarifaZaKielektronikiPage

--- lib/pages/profile/taarifa_za_kielektronic.dart
+++ lib/pages/profile/taarifa_za_kielektronic.dart
@@ -35,113 +35,125 @@ class _TaarifaZaKielektronikiPageState
         itemCount: 1,
         itemBuilder: (BuildContext context, int index) {
           return Container(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  color: Color(0xfff1f1f1),
-                  width: double.infinity,
-                  padding: EdgeInsets.symmetric(vertical: 30, horizontal: 30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 10),
-                        child: Text(
-                          'Kuanzia Tarehe',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                              color: Color(0xff76767a)),
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            'Kuanzia Tarehe',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Color(0xff76767a)),
+                          ),
                         ),
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                            hintText: 'mfano: 16/01/2020',
-                            hintStyle: TextStyle(
-                              color: Color(0xff959599),
-                              fontSize: 16,
-                            ),
-                            filled: true,
-                            fillColor: Colors.white,
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(color: Colors.grey)),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(
-                                  color: Theme.of(context).primaryColor),
-                            )),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 10, top: 20),
-                        child: Text(
-                          'Muda',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                              color: Color(0xff76767a)),
+                        TextField(
+                          decoration: InputDecoration(
+                              hintText: 'mfano: 16/01/2020',
+                              hintStyle: TextStyle(
+                                color: Color(0xff959599),
+                                fontSize: 16,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(0),
+                                  borderSide: BorderSide(color: Colors.grey)),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(0),
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor),
+                              )),
                         ),
-                      ),
-                      DropdownButtonFormField(
-                        decoration: InputDecoration(
-                            hintText: 'Miezi 3',
-                            hintStyle: TextStyle(
-                              color: Color(0xff959599),
-                              fontSize: 16,
-                            ),
-                            filled: true,
-                            fillColor: Colors.white,
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(color: Colors.grey)),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(
-                                  color: Theme.of(context).primaryColor),
-                            )),
-                        items: ['Miezi 3', 'Miezi 6', 'Miezi 9', 'Mwaka 1']
-                            .map((String value) {
-                          return DropdownMenuItem(
-                              value: value, child: Text(value));
-                        }).toList(),
-                        onChanged: (_) {},
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 10, top: 20),
-                        child: Text(
-                          'Barua Pepe',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                              color: Color(0xff76767a)),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 10, top: 20),
+                          child: Text(
+                            'Muda',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Color(0xff76767a)),
+                          ),
                         ),
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                            hintText: 'mfano: johndoe@gmail.com',
-                            hintStyle: TextStyle(
-                              color: Color(0xff959599),
-                              fontSize: 16,
-                            ),
-                            filled: true,
-                            fillColor: Colors.white,
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(color: Colors.grey)),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(
-                                  color: Theme.of(context).primaryColor),
-                            )),
-                      ),
-                    ],
+                        DropdownButtonFormField(
+                          decoration: InputDecoration(
+                              hintText: 'Miezi 3',
+                              hintStyle: TextStyle(
+                                color: Color(0xff959599),
+                                fontSize: 16,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(0),
+                                  borderSide: BorderSide(color: Colors.grey)),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(0),
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor),
+                              )),
+                          items: ['Miezi 3', 'Miezi 6', 'Miezi 9', 'Mwaka 1']
+                              .map((String value) {
+                            return DropdownMenuItem(
+                                value: value, child: Text(value));
+                          }).toList(),
+                          onChanged: (_) {},
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 10, top: 20),
+                          child: Text(
+                            'Barua Pepe',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Color(0xff76767a)),
+                          ),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              hintText: 'mfano: johndoe@gmail.com',
+                              hintStyle: TextStyle(
+                                color: Color(0xff959599),
+                                fontSize: 16,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                  borderSide: BorderSide(color: Colors.grey)),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5),
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor),
+                              )),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                FlatButton(
-                  onPressed: null,
-                  child: Text('Tuma'),
-                )
-              ],
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: FlatButton(
+                      color: Theme.of(context).accentColor,
+                      textColor: Colors.white,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                      splashColor: Theme.of(context).primaryColor,
+                      onPressed: () {},
+                      child: Text(
+                        "WASILISHA",
+                        style: TextStyle(fontSize: 16.0, letterSpacing: 2),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           );
         },
